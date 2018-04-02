13612dfead7fabf123a59fefb33d6f046b44affc
naegahyung <naegahyung@gmail.com>
2018-04-02 15:33:38 -0400

    changed the port configuration

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
-const PORT = 5000 || process.env.PORT;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Listening to ${PORT}`);
 });
